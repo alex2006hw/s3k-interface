1a6f3547c600f32edc6a29b07c6bfe5971a2c618
amazingandyyy <amazingandyyy@gmail.com>
2016-10-22 15:31:51 -0700

    add gulp
    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,49 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 
-var rimraf = require('gulp-rimraf');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var plumber = require('gulp-plumber');
-var cleanCSS = require('gulp-clean-css');
-var sourcemaps = require('gulp-sourcemaps');
-var plumber = require('gulp-plumber');
+const rimraf = require('gulp-rimraf');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
+const react = require('gulp-react');
+const babel = require('gulp-babel');
 
-gulp.task('default', ['css', 'watch']);
+gulp.task('default', ['js', 'css', 'watch']);
 
 gulp.task('watch', function () {
     gulp.watch('public/**/*.scss', ['css']);
+    gulp.watch('public/**/*.jsx', ['js']);
 });
 
+gulp.task('js', ['clean:js'], function () {
+    return gulp.src(['public/**/*.jsx'])
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('clean:js', function () {
+    return gulp.src('public/js/**/*.js', {
+        read: false
+    })
+        .pipe(rimraf());
+});
+
+
 gulp.task('css', ['clean:css'], function () {
-    return gulp.src(['public/stylesheets/**/*.scss'])
+    return gulp.src(['public/style/**/*.scss'])
         .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(cleanCSS())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('public/stylesheets'));
+        .pipe(gulp.dest('public/style'));
 });
 
 gulp.task('clean:css', function () {
