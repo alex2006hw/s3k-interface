0b5dd878ef36d40e344af44bcba1e983dfeac716
amazingandyyy <amazingandyyy@gmail.com>
2016-10-22 11:08:50 -0700

    add react
    add react

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+var MainComponent = React.createClass({
+    render: function(){
+        return (
+           <div>start</div>
+        );
+    }
+});
+
+ReactDOM.render(<MainComponent name="main" />, document.getElementById('main'));
