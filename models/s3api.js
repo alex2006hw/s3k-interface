60ed5255c75e21ed48f8ff79aba57996ff113a50
amazingandyyy <amazingandyyy@gmail.com>
2016-10-23 21:06:46 -0700

    up to date
    up to date

--- models/s3api.js
+++ models/s3api.js
@@ -22,7 +22,7 @@ let configPath = path.join(__dirname, '..', "config.json");
  		},
  		function(err, data) { // Upload file to S3
  			s3.putObject({
- 				Bucket: 's3', //Bucket Name
+ 				Bucket: 'demo-yo', //Bucket Name
  				Key: file.originalname, //Upload File Name, Default the original name
  				Body: data
  			}, this);
